b8f4acbb8128192c1213fd08e486853e2db36de9
githubala <balachandra.kun@gmail.com>
2024-04-11 08:07:01 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// run `node index.js` in the terminal
+
+//console.log(`Hello Node.js v${process.versions.node}!`);
+
+// const fs = require('fs');
+
+// fs.writeFileSync('notes.txt', 'the is my first nodejs coding ');
+// fs.appendFileSync(
+//   'notes.txt',
+//   'and i would be able to finish the course on time'
+// );
+//const chalk = require('chalk');
+const notes = require('./notes.js');
+
+//const msg = chalk.green('success');
+console.log(notes);
+//console.log(msg);
