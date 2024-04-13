c871ac1a198dcf7540e355e9fadcdb209cb64bec
githubala <balachandra.kun@gmail.com>
2024-04-13 23:00:03 +0530

    changing data

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 // run `node index.js` in the terminal
 
-const command = require("nodemon/lib/config/command");
-const yargs = require('yargs')
-
 //console.log(`Hello Node.js v${process.versions.node}!`);
 
 // const fs = require('fs');
@@ -12,50 +9,10 @@ const yargs = require('yargs')
 //   'notes.txt',
 //   'and i would be able to finish the course on time'
 // );
-// const chalk = require('chalk');
-// const validator = require('validator');
-// const notes = require('./notes.js');
+//const chalk = require('./node_modules/chalk/source/index.js');
+import chalk from "chalk";
+const notes = require('./notes.js');
 
-// const msg = chalk.bgBlueBright.black.inverse.bold('success');
-// console.log(validator.isEmail('bala@gmail.com'))
-// console.log(notes);
-// console.log(msg);
-
-// console.log(process.argv);
-// const command1 = process.argv[2]
-
-// if(command1 == 'bala'){
-//     console.log('my name is '+command1);
-// }
-
-// console.log(yargs.argv)
-
-yargs.command({
-    command:'add',
-    describe:'add the note',
-    handler: function(){
-        console.log('this is a add note function ')
-    }
-})
-yargs.parse();
-//console.log(yargs.argv);
-
-//create remove command
-yargs.command({
-    command:'remove',
-    describe:'remove the note',
-    builder:{
-        title: {
-        describe:'removing the note',
-        demandOption:true,
-        //type:'string'
-        }
-    },
-    handler: function (argv) {
-        //console.log('remove the node  ', argv)
-        console.log('title: '+argv.title)
-    }
-})
-
-//console.log(yargs.argv);
-yargs.parse();
+const msg = chalk.green('success');
+console.log(notes);
+console.log(msg);
--- playground/1-json.js
+++ playground/1-json.js
@@ -1,3 +1,14 @@
 const fs = require('fs')
 
-const dataBuffer = fs.readFileSync('')
+const dataBuffer = fs.readFileSync('1-json.json');
+
+console.log(dataBuffer); 
+const dataJSON = dataBuffer.toString() //JSON.stringify(dataBuffer);
+const user = JSON.parse(dataJSON)
+console.log(user)
+user.name = 'bala';
+user.age = 41
+
+const userJSON = JSON.stringify(user)
+console.log(userJSON)
+fs.writeFileSync('1-json.json', userJSON)
