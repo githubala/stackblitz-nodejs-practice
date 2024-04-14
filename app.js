20c10e9f358da00bd186ca7e04b17153a9fa6d95
githubala <balachandra.kun@gmail.com>
2024-04-14 20:44:00 +0530

    implemented the yargs

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
+//const add = require('./utils');
 const sum = require('./utils')
 
-console.log(sum(3,4));
+//console.log(sum(3,4));
 
+const yargs = require('yargs')
 
+yargs.command({
+    command:'add',
+    describe:'add the node',
+    builder:{
+        title: {
+           describe:'adding title of the note' ,
+           demandOption:true,
+           type:'string'
+        },
+        body: {
+            describe:'adding body of the note',
+           demandOption:false,
+           type:'string'
+        }
+    },
+    handler:function(argv){
+     console.log('adding the node', argv);
+    }
+})
 
+yargs.parse();
