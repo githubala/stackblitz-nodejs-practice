c46bbcc3f74b3724ee596526604892b046e9c0ad
githubala <balachandra.kun@gmail.com>
2024-04-15 15:35:34 +0530

    validator and chalk npms

--- playground/1-json.js
+++ playground/1-json.js
@@ -4,9 +4,6 @@ const dataBuffer = fs.readFileSync('1-json.json');
 
 console.log(dataBuffer); 
 const dataJSON = dataBuffer.toString() //JSON.stringify(dataBuffer);
-//to convert buffer data into object we have used toString in the above line
-//since after reading the file using readFileSync we gets data like 7b 22 32 45 4r we have to use the
-//toString function
 console.log(dataJSON)
 const user = JSON.parse(dataJSON)
 console.log(user)
